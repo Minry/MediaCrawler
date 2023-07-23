66579287c0f0ee64885292fb7490feda3558fd2d
Minry <1106634551@qq.com>
2023-07-24 01:45:05 +0800

    测试多个cookie

--- config/base_config.py
+++ config/base_config.py
@@ -4,7 +4,9 @@ LOGIN_TYPE = "qrcode"  # qrcode or phone or cookies
 LOGIN_TYPE_COOKIE = "cookies"  # qrcode or phone or cookies
 # If it's on the Xiaohongshu platform, only the web_session cookie will be kept.
 # xhs cookie format -> web_session=040069b2acxxxxxxxxxxxxxxxxxxxx;
-COOKIES = "web_session=030037a3824fcc373b5ef45dc9234a4ebc975f;"
+# COOKIES = "web_session=030037a3824fcc373b5ef45dc9234a4ebc975f;"
+COOKIES = "web_session=030037a389786f37e817576ac2234a0d234b14;"
+# COOKIES = "web_session=030037a38976af3724359764c2234ac9d51e5c;"
 
 # redis config
 REDIS_DB_HOST = "redis://127.0.0.1"  # your redis host
